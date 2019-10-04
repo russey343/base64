bff05b7488cb037ef0b4d187f1c0d280156b78e8
Raymond So <rso@ouhk.edu.hk>
2019-10-04 15:03:17 +0800

    used more ES6 syntax

--- example1/server.js
+++ example1/server.js
@@ -1,20 +1,23 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var formidable = require('formidable');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const formidable = require('formidable');
 
-var server = http.createServer(function (req, res) {
-  var parsedURL = url.parse(req.url,true);
+const server = http.createServer((req, res) => {
+  let timestamp = new Date().toISOString();
+	console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
+
+	let parsedURL = url.parse(req.url,true); // true to get query as object 
   
   if (parsedURL.pathname == '/fileupload' && 
       req.method.toLowerCase() == "post") {
     // parse a file upload
-    var form = new formidable.IncomingForm();
+    const form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      var filename = files.filetoupload.path;
-      console.log("filename = " + filename);
-      fs.readFile(filename, function(err,data) {
-         var base64 = new Buffer(data).toString('base64');
+      console.log(`filename =  ${files.filetoupload.path}`);
+      console.log(`type: ${files.filetoupload.type}`);
+      fs.readFile(files.filetoupload.path, (err,data) => {
+         const base64 = new Buffer(data).toString('base64');
          res.writeHead(200,{"Content-Type": "text/plain"});
          res.write('File uploaded: (Base64)\n');         
          res.end(base64);
